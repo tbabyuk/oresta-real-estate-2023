7facafebca749455c99ed7c04daafcf37c9053ad
Terry Babyuk <tbabyuk@gmail.com>
2023-09-16 12:36:30 -0400

    updated webinar zoom link to Oresta's

--- app/components/WebinarForm.jsx
+++ app/components/WebinarForm.jsx
@@ -36,14 +36,14 @@ export const WebinarForm = () => {
   return (
     <>
     {sent ? 
-      (<p className="text-lg text-green-500">Congratulations - you have successfully registered for the Webinar!<br /> Please use the following link to join the webinar: <a href="https://us02web.zoom.us/webinar/register/WN_luspGAoWRwSUY8mwq5MylA#/registration" target="_blank" className="text-gray-50 hover:underline">https://us02web.zoom.us/webinar/register/WN_luspGAoWRwSUY8mwq5MylA#/registration</a></p>) : 
+      (<p className="text-lg text-green-500">Congratulations - you have successfully registered for the Webinar!<br /> Please use the following link to join the webinar: <a href="https://us06web.zoom.us/j/84742986150" target="_blank" className="text-gray-50 hover:underline">https://us06web.zoom.us/j/84742986150</a></p>) : 
       (<form className="w-[330px] mx-auto flex flex-col" onSubmit={handleSubmit}>
           <label className="mb-10 flex flex-col">
-              <span className="text-gray-50 text-lg font-light self-start">Name:</span>
+              <span className="text-gray-50 font-light self-start mb-2">Name:</span>
               <input type="text" className="h-8 rounded ps-2" required ref={nameRef} />
           </label>
           <label className="mb-14 flex flex-col">
-              <span className="text-gray-50 text-lg font-light self-start">Email:</span>
+              <span className="text-gray-50 font-light self-start mb-2">Email:</span>
               <input type="email" className="h-8 rounded ps-2" required ref={emailRef} />
           </label>
           <button className="bg-orange-400 text-gray-100 w-full h-12 mx-auto px-14 rounded hover:bg-orange-500" disabled={sending}>{sending ? "processing reservation..." : "Reserve My Spot!"}</button>
