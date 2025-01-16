64dbbf4e25124f2b189ef6d4cce661436ed17671
Terry Babyuk <tbabyuk@gmail.com>
2025-01-15 23:16:39 -0500

    added foundations-of-wealth landing page with embedded Google Form

---
+++ app/(landing)/foundations-of-wealth/page.jsx
@@ -0,0 +1,16 @@
+
+
+
+const FoundationsOfWealthLandingPage = () => {
+
+
+  return (
+    <div className="flex justify-center bg-oresta-light">
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfMHDYb1mULOiUn5QaygDcMc88o-fghsiKgjrx1xT58VIEHrA/viewform?embedded=true" width="640" height="1156" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+    </div>
+  )
+}
+{/* <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11566.196608402468!2d-79.5833131!3d43.5534391!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b47e2eef44ba3%3A0x13cbbe5d0f33d02f!2sOresta%20Kisil%20Real%20Estate%20Services!5e0!3m2!1sen!2sca!4v1684509067581!5m2!1sen!2sca" width="100%" height={450} style={{ border: 0 }} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"/> */}
+
+export default FoundationsOfWealthLandingPage
+
