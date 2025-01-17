b81b1872340ae0d0d0c82ff561ac10e0dd6d2045
Terry Babyuk <tbabyuk@gmail.com>
2025-01-16 23:44:44 -0500

    improved registration form ui with input auto focus and custom input focus colors, improved success and fail message styling

--- app/(landing)/foundations-of-wealth/page.jsx
+++ app/(landing)/foundations-of-wealth/page.jsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState, useRef } from "react"
+import { useState, useEffect, useRef } from "react"
 
 
 
@@ -50,17 +50,21 @@ const FoundationsOfWealthLandingPage = () => {
               setSending(false)
         }
     }
-  
 
+  
+    useEffect(() => {
+      firstNameRef.current?.focus()
+    }, [])
+  
 
   return (
       <div className="bg-oresta-light min-h-[100vh] py-14">
           {successMessage && 
-              (<p className="text-center text-xl text-green-700">Thank you - your form has been submitted successfully!<br /> You should be receiving your e-tickets within 24 hours!</p>)
+              (<p className="mt-6 bg-white w-fit mx-auto p-8 rounded text-center text-xl text-green-600">Thank you - you have been registered!</p>)
           }
 
           {failMessage && 
-              (<p className="text-center text-xl text-red-700">Ooops, something went wrong! Please try again or call Oresta directly at <a href="tel:+14163195748" className="font-bold">(416) 319-5748</a> to ask for tickets!</p>)
+              (<p className="mt-6 bg-white w-fit mx-auto p-8 rounded text-center text-xl text-red-600">Ooops, something went wrong! Please try again or call Oresta directly at <a href="tel:+14163195748" className="font-bold">(416) 319-5748</a> to ask for tickets!</p>)
           }
 
           {!successMessage && !failMessage && 
@@ -69,23 +73,23 @@ const FoundationsOfWealthLandingPage = () => {
                   <p className="text-sm mb-8">Learn the foundational tools for saving for your first home and investing in the stock market!</p>
                   <label className="mb-8 flex flex-col">
                       <span className="self-start mb-2">First Name:</span>
-                      <input type="text" className="bg-oresta-light/20 h-9 rounded ps-2 shadow" required ref={firstNameRef} />
+                      <input type="text" className="bg-gray-100 h-9 rounded ps-2 shadow focus:outline-none focus:ring-2 focus:ring-oresta-light" required ref={firstNameRef} />
                   </label>
                   <label className="mb-8 flex flex-col">
                       <span className="self-start mb-2">Last Name:</span>
-                      <input type="text" className="bg-oresta-light/20 h-9 rounded ps-2 shadow" required ref={lastNameRef} />
+                      <input type="text" className="bg-gray-100 h-9 rounded ps-2 shadow focus:outline-none focus:ring-2 focus:ring-oresta-light" required ref={lastNameRef} />
                   </label>
                   <label className="mb-8 flex flex-col">
                       <span className="self-start mb-2">Phone Number:</span>
-                      <input type="tel" className="bg-oresta-light/20 h-9 rounded ps-2 shadow" required ref={phoneRef} />
+                      <input type="tel" className="bg-gray-100 h-9 rounded ps-2 shadow focus:outline-none focus:ring-2 focus:ring-oresta-light" required ref={phoneRef} />
                   </label>
                   <label className="mb-8 flex flex-col">
                       <span className="self-start mb-2">Email:</span>
-                      <input type="email" className="bg-oresta-light/20 h-9 rounded ps-2 shadow" required ref={emailRef} />
+                      <input type="email" className="bg-gray-100 h-9 rounded ps-2 shadow focus:outline-none focus:ring-2 focus:ring-oresta-light" required ref={emailRef} />
                   </label>
                   <label className="mb-12 flex flex-col">
                       <span className="self-start mb-2">Number of Tickets:</span>
-                      <select className="bg-oresta-light/20 h-9 rounded ps-3 shadow" ref={numTicketsRef}>
+                      <select className="bg-gray-100 h-9 rounded ps-3 shadow focus:outline-none focus:ring-2 focus:ring-oresta-light" ref={numTicketsRef}>
                           <option value="1">1</option>
                           <option value="2">2</option>
                           <option value="3">3</option>
@@ -93,7 +97,7 @@ const FoundationsOfWealthLandingPage = () => {
                           <option value="5">5</option>
                       </select>
                   </label>
-                  <button className="bg-orange-500 text-gray-100 w-full h-12 mx-auto px-14 rounded hover:bg-orange-600" disabled={sending}>{sending ? "processing, please wait..." : "Get My Tickets!"}</button>
+                  <button className="bg-orange-500 text-gray-100 w-full h-12 mx-auto px-14 rounded hover:bg-orange-600  focus:outline-none focus:ring-2 focus:ring-oresta-light" disabled={sending}>{sending ? "processing, please wait..." : "Get My Tickets!"}</button>
               </form>)
           }
       </div>
