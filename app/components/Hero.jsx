db32a02a9af40a2633478d531ce9144c05388e35
Terry Babyuk <tbabyuk@gmail.com>
2025-03-27 01:37:52 -0400

    removed logo from hero section, re-organized content with CTA at the bottom center

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -10,16 +10,12 @@ export const Hero = () => {
             <img src="/images/oresta_banner_image.png" alt="photo of Oresta Kisil" className="w-[210px]" />
         </div>
         <div className="hidden lg:flex lg:flex-col px-8 font-light text-oresta-blue/70 self-center">
-            <h1 className="text-3xl xl:text-4xl font-semibold pb-4 mb-1">Oresta Kisil | Your west-end Toronto and GTA Realtor</h1>
-            <ul className="list-inside list-disc space-y-3 font-normal text-xl xl:text-2xl">
+            <h1 className="text-3xl xl:text-4xl font-semibold mb-5">Oresta Kisil | Your west-end Toronto and GTA Realtor</h1>
+            <ul className="list-inside list-disc font-normal text-xl xl:text-2xl mb-7">
                 <li>Bringing ease and confidence to your real estate journey</li>
                 <li>Supporting sellers, buyers, leasees & investors since 2015</li>
             </ul>
-        </div>
-        <div className="w-[50%] lg:w-auto flex lg:flex-shrink-0 flex-col items-center justify-evenly">
-            <Link href="/" className="block h-fit"><img src="/images/logo_white_transparent.png" alt="Oresta Kisil Real Estate logo" className="flex-shrink-0 bg-black/20 rounded-xl h-[85px] sm:h-[120px] lg:h-[140px]" /></Link>
-            <h1 className="lg:hidden px-3 text-center text-xl">Find your Dream Home in Toronto & the GTA</h1>
-            <Link href="/contact#contact-form"><button className="oresta-action-orange w-[170px] h-[45px] lg:w-[290px] lg:h-[60px] lg:text-xl">Get In Touch</button></Link>
+            <Link href="/contact#contact-form" className="mx-auto"><button className="oresta-action-orange w-[170px] h-[45px] lg:w-[290px] lg:h-[60px] lg:text-xl font-semibold">Get In Touch</button></Link>
         </div>
     </div>
   )
