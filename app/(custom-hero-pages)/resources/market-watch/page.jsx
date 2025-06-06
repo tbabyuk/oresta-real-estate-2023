db92aefcdbc81898b8777a8f2504eceee3929c01
Terry Babyuk <tbabyuk@gmail.com>
2025-06-06 09:45:07 -0400

    added May market watch to /resources/market-watch, changed links from text to button-style

--- app/(custom-hero-pages)/resources/market-watch/page.jsx
+++ app/(custom-hero-pages)/resources/market-watch/page.jsx
@@ -9,12 +9,17 @@ const MarketWatchPage = () => {
             headerClass="bg-[url('/images/pages/market-watch/market-watch-header-bg.png')]" 
             headerTitle="Market Watch"
         />
-        <div className="px-5 py-20 flex justify-center items-center bg-cover">
-            <div className="max-w-[500px] rounded-xl overflow-hidden">
-                <a href="/documents/market_watch_april_2025.pdf" className="oresta-link" download>
+        <div className="px-5 py-20 flex justify-center items-center">
+
+            <div className="flex flex-col space-y-6 rounded-xl">
+                <a href="/documents/market_watch_april_2025.pdf" className="w-fit block text-center px-4 py-3 rounded text-white bg-oresta-orange" download>
                     Download April 2025 Market Watch Report
                 </a>
-          </div>
+                <a href="/documents/market_watch_may_2025.pdf" className="w-fit block text-center px-4 py-3 rounded text-white bg-oresta-orange" download>
+                    Download May 2025 Market Watch Report
+                </a>
+           </div>
+
         </div>
     </main>  
   )
