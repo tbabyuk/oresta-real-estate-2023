30f9de9291ef09dcc344d33ca0850ee3b2b08448
Terry Babyuk <tbabyuk@gmail.com>
2025-07-08 11:57:52 -0400

    added June 2025 market watch download link

--- app/(custom-hero-pages)/resources/market-watch/page.jsx
+++ app/(custom-hero-pages)/resources/market-watch/page.jsx
@@ -11,12 +11,15 @@ const MarketWatchPage = () => {
         />
         <div className="px-5 py-20 flex justify-center items-center">
 
-            <div className="flex flex-col space-y-6 rounded-xl">
-                <a href="/documents/market_watch_april_2025.pdf" className="w-fit block text-center px-4 py-3 rounded text-white bg-oresta-orange" download>
+            <div className="flex flex-col space-y-6 rounded-xl text-[0.9rem]">
+                <a href="/documents/market_watch_april_2025.pdf" className="w-fit block text-center px-4 py-3 rounded oresta-action-orange" download>
                     Download April 2025 Market Watch Report
                 </a>
-                <a href="/documents/market_watch_may_2025.pdf" className="w-fit block text-center px-4 py-3 rounded text-white bg-oresta-orange" download>
+                <a href="/documents/market_watch_may_2025.pdf" className="w-fit block text-center px-4 py-3 rounded oresta-action-orange" download>
                     Download May 2025 Market Watch Report
+                </a>
+                <a href="/documents/market_watch_june_2025.pdf" className="w-fit block text-center px-4 py-3 rounded oresta-action-orange" download>
+                    Download June 2025 Market Watch Report
                 </a>
            </div>
 
