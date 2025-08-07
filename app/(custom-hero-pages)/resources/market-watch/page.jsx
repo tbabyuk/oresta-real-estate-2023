785bf530312d650819abc89857e35cb2fec21646
Terry Babyuk <tbabyuk@gmail.com>
2025-08-07 01:47:47 -0400

    added July 2025 Market Watch pdf

--- app/(custom-hero-pages)/resources/market-watch/page.jsx
+++ app/(custom-hero-pages)/resources/market-watch/page.jsx
@@ -3,6 +3,28 @@ import { CustomHeader } from "../../components/CustomHeader"
 
 
 const MarketWatchPage = () => {
+
+  const marketWatchReports = [
+    {
+        title: "July 2025 Market Watch Report",
+        link: "/documents/market_watch_july_2025.pdf"
+    },
+    {
+        title: "June 2025 Market Watch Report",
+        link: "/documents/market_watch_june_2025.pdf"
+    },
+    {
+        title: "May 2025 Market Watch Report",
+        link: "/documents/market_watch_may_2025.pdf"
+    },
+    {
+      title: "April 2025 Market Watch Report",
+      link: "/documents/market_watch_april_2025.pdf"
+    }
+  ]
+
+
+
   return (
     <main>
         <CustomHeader 
@@ -10,19 +32,13 @@ const MarketWatchPage = () => {
             headerTitle="Market Watch"
         />
         <div className="px-5 py-20 flex justify-center items-center">
-
             <div className="flex flex-col space-y-6 rounded-xl text-[0.9rem]">
-                <a href="/documents/market_watch_april_2025.pdf" className="w-fit block text-center px-4 py-3 rounded oresta-action-orange" download>
-                    Download April 2025 Market Watch Report
-                </a>
-                <a href="/documents/market_watch_may_2025.pdf" className="w-fit block text-center px-4 py-3 rounded oresta-action-orange" download>
-                    Download May 2025 Market Watch Report
-                </a>
-                <a href="/documents/market_watch_june_2025.pdf" className="w-fit block text-center px-4 py-3 rounded oresta-action-orange" download>
-                    Download June 2025 Market Watch Report
-                </a>
+                {marketWatchReports.map((report, index) => (
+                    <a href={report.link} key={index} className="min-w-[220px] block text-center px-4 py-3 rounded oresta-action-orange" download>
+                        {report.title}
+                    </a>
+                ))}
            </div>
-
         </div>
     </main>  
   )
